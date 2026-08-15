25fb2c59b7e449735396da76eac3db6937c00056
Steven Zamorano <stevenzamorano.code@gmail.com>
2026-08-15 17:10:04 -0400

    Render the component.

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,5 +1,9 @@
+import { render } from "@testing-library/react";
+import Greeting from "./Greeting";
+
 test("renders Hello World as a text", () => {
   //Arrange
+  render(<Greeting />);
   //Act
   //Assert
 });
