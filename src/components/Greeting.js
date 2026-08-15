30dd2a085e49dd0ec7dbc96132a860e4cf1e375f
Steven Zamorano <stevenzamorano.code@gmail.com>
2026-08-15 19:46:26 -0400

    Test user interaction and state.

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,8 +1,19 @@
+import { useState } from "react";
+
 function Greeting() {
+  const [changedText, setChangedText] = useState(false);
+
+  function changeTextHandler() {
+    setChangedText(true);
+  }
+
   return (
     <div>
       <h2>Hello World!</h2>
+      {!changedText && <p>It's good to see you!</p>}
+      {changedText && <p>Changed!</p>}
       <p>It's good to see you!</p>
+      <button onClick={changeTextHandler}>Change Text!</button>
     </div>
   );
 }
